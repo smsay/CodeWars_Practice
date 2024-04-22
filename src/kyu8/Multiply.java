10856a274d76f626728eba417e867fac89fa7e7a
Yevgeniy5056 <149609362+Yevgeniy5056@users.noreply.github.com>
2024-04-21 22:32:33 -0400

    Solved

--- src/kyu8/Multiply.java
+++ src/kyu8/Multiply.java
@@ -4,12 +4,21 @@ package kyu8;
 /**
  * Given a buggy code to multiply two longs,
  * Kata requires one to fix the bug
- *
+ * <p>
  * RCA: Missing semi-colon at the end of statement;
- *
+ * <p>
  * Category: Bugs
  * https://www.codewars.com/kata/50654ddff44f800200000004
  */
 public class Multiply {
 
+    public static void main(String[] args) {
+
+        System.out.println(multiply(1.0, 1.0));
+        System.out.println(multiply(2.0, 1.0));
+    }
+
+    static Double multiply(Double a, Double b) {
+        return a * b;
+    }
 }
