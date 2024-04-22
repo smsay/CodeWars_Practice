e6f569750ecde01cbd14a647228cd0b6b5d7024a
Yevgeniy5056 <149609362+Yevgeniy5056@users.noreply.github.com>
2024-04-21 23:08:11 -0400

    Solved

--- src/kyu8/MixedSum.java
+++ src/kyu8/MixedSum.java
@@ -13,5 +13,20 @@ Categories : Fundamentals, Strings, Numbers, Arrays
  */
 public class MixedSum {
 
+    public static void main(String[] args) {
+
+        var array = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        System.out.println(YevgeniySolutionMixedSum(array));
+    }
+
+    static int YevgeniySolutionMixedSum(String[] arr) {
+
+        var sum = 0;
+
+        for (var e : arr) {
+            sum += Integer.parseInt(e);
+        }
+        return sum;
+    }
 
 }
