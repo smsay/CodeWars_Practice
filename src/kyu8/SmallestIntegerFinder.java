abb729531529c6d3f9169f1f825e83a8ab693f53
Yevgeniy5056 <149609362+Yevgeniy5056@users.noreply.github.com>
2024-04-22 22:14:57 -0400

    Solved

--- src/kyu8/SmallestIntegerFinder.java
+++ src/kyu8/SmallestIntegerFinder.java
@@ -1,7 +1,6 @@
 package kyu8;
 
 
-
 /*
 Find the smallest integer in the array
 
@@ -21,5 +20,25 @@ Categories : Fundamentals
  */
 public class SmallestIntegerFinder {
 
+    public static void main(String[] args) {
 
+        var array = new int[]{34, 15, 88, 2};
+        var array2 = new int[]{34, -345, -1, 100};
+        System.out.println(YevgeniySolutionFindSmallest(array));
+        System.out.println(YevgeniySolutionFindSmallest(array2));
+
+
+    }
+
+    static int YevgeniySolutionFindSmallest(int[] arr) {
+
+        var smallest = arr[0];
+
+        for (var e : arr) {
+            if (e < smallest) {
+                smallest = e;
+            }
+        }
+        return smallest;
+    }
 }
