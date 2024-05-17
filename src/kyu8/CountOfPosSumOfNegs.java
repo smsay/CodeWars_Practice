b8e869d9f9a953a01964428fdbff1b91647c849c
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-05-17 09:22:25 +0300

    CountOfPosSumOfNegs is solved

--- src/kyu8/CountOfPosSumOfNegs.java
+++ src/kyu8/CountOfPosSumOfNegs.java
@@ -1,5 +1,6 @@
 package kyu8;
 
+import java.util.Arrays;
 
 /*
 Count of positives / sum of negatives
@@ -17,5 +18,18 @@ Categories : Fundamentals, Arrays, Lists, Data Structures
 Arithmetic, Mathematics, Algorithms, Numbers
  */
 public class CountOfPosSumOfNegs {
-
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15};
+        System.out.println(Arrays.toString(count(arr)));
+    }
+    static int[] count(int[] arr) {
+        if (arr.length==0) return new int[0];
+        int neg = 0;
+        int poz = 0;
+        for (int c : arr) {
+            if(c<0) neg+=c;
+            else if (c>0) poz++;
+        }
+        return new int[] {poz, neg};
+    }
 }
