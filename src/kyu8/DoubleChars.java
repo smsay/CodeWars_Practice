9392bc6e0dc98b1ced38229e49fe755b5ea2130e
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-05-21 22:46:26 +0300

    DoubleChars is solved

--- src/kyu8/DoubleChars.java
+++ src/kyu8/DoubleChars.java
@@ -16,5 +16,32 @@ Categories :  Fundamentals, Loops, Control Flow, Basic Language Features
 
  */
 public class DoubleChars {
+    public static void main(String[] args) {
+        String s ="String";
+        String s1 ="Hello World";
+        String s2 ="1234!_ ";
+        System.out.println(doubleChar(s));
+        System.out.println(doubleChar(s1));
+        System.out.println(doubleChar(s2));
+    }
+    public static String doubleCharOpt(String str) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            result.append(c).append(c);
+        }
+
+        return result.toString();
+    }
+    public static String doubleChar(String s) {
+        String result="";
+        for (int i = 0; i < s.length(); i++) {
+            result+=s.charAt(i);
+            result+=s.charAt(i);
+        }
+        return result;
+    }
+
 
 }
--- src/kyu8/EvenNumbers.java
+++ src/kyu8/EvenNumbers.java
@@ -1,11 +1,14 @@
 package kyu8;
 
 
+import java.util.ArrayList;
+import java.util.List;
 
 /*
 Find numbers which are divisible by given number
 
-Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
+Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor.
+First argument is an array of numbers and the second is the divisor.
 Example
 
 divisibleBy([1, 2, 3, 4, 5, 6], 2) == [2, 4, 6]
@@ -13,6 +16,20 @@ divisibleBy([1, 2, 3, 4, 5, 6], 2) == [2, 4, 6]
 Categories : Algorithms, Arrays
  */
 public class EvenNumbers {
-
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int divisor = 2;
+        System.out.println(divisibleBy(arr, divisor));
+    }
+    public static List<Integer> divisibleBy(int[] arr, int divisor) {
+        List<Integer> arrayList = new ArrayList<>();
+        int[] result= new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]%divisor==0) {
+                arrayList.add(arr[i]);
+            }
+        }
+        return arrayList;
+    }
 
 }
