f3361be9187bca4b481e18b4df88623229d44289
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-05-21 22:46:07 +0300

    DingleMouse is solved

--- src/kyu8/Dinglemouse.java
+++ src/kyu8/Dinglemouse.java
@@ -1,6 +1,7 @@
 package kyu8;
 
 
+import java.util.Arrays;
 
 /*
 Cat years, Dog years
@@ -29,7 +30,49 @@ References
 Categories : Fundamentals
  */
 public class Dinglemouse {
+    public static void main(String[] args) {
 
+        int a=5;
+        System.out.println(Arrays.toString(convertYear(a))); //[humanYears,catYears,dogYears]
+        System.out.println(Arrays.toString(calculateAges(5)));
+    }
 
+    public static int[] convertYear(int a) {
+        int c, d = 0;
+
+     if (a>=1&&a<2) {
+         c=15;
+         d=15;
+     } else if (a>=2&&a<3) {
+         c=15+9;
+         d=15+9;
+     } else  {
+         c=15+9+(4*(a-2));
+         d=15+9+(5*(a-2));
+     }
+     return new int[]{a,c,d};
+    }
+
+    public static int[] calculateAges(int humanYears) {
+        int catYears = 0;
+        int dogYears = 0;
+
+        if (humanYears >= 1) {
+            if (humanYears >= 1) {
+                catYears += 15;
+                dogYears += 15;
+            }
+            if (humanYears >= 2) {
+                catYears += 9;
+                dogYears += 9;
+            }
+            if (humanYears > 2) {
+                catYears += (humanYears - 2) * 4;
+                dogYears += (humanYears - 2) * 5;
+            }
+        }
+
+        return new int[] {humanYears, catYears, dogYears};
+    }
 
 }
