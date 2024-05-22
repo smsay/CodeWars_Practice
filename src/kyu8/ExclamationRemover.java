ecb08c34fc579f0aade8222b9c7c90b4eaae6231
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-05-22 07:13:55 +0300

    ExclamationRemover is solved

--- src/kyu8/ExclamationRemover.java
+++ src/kyu8/ExclamationRemover.java
@@ -11,5 +11,18 @@ Categories : Fundamentals
  */
 class ExclamationRemover {
 
+    public static void main(String[] args) {
+        System.out.println(removeExclamation("!!!ytghhgh!dffdfg!!."));
+    }
+
+    public static String removeExclamation(String s) {
+        StringBuilder result =new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) !='!') {
+                result.append(s.charAt(i));
+            }
+        }
+        return result.toString();
+    }
 
 }
