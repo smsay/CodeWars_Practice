156e94583b46ee97f460bc3e92fd637baf1404ee
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-05-22 07:13:44 +0300

    EvenOrOdd is solved

--- src/kyu8/EvenOrOdd.java
+++ src/kyu8/EvenOrOdd.java
@@ -11,5 +11,12 @@ and returns "Even" for even numbers or "Odd" for odd numbers.
 Categories : Fundamentals, Mathematics, Algorithms, Numbers
  */
 public class EvenOrOdd {
+    public static void main(String[] args) {
+        System.out.println(oddOrEven(21));
+    }
 
+    public static String oddOrEven(int a) {
+        if (a%2==0) return "Even";
+        return "Odd";
+    }
 }
