2208ef2c6850ab46b8d46a103a00c612feadfdd0
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-05-22 07:14:21 +0300

    FakeBinary is solved

--- src/kyu8/FakeBinary.java
+++ src/kyu8/FakeBinary.java
@@ -14,5 +14,20 @@ Categories : Fundamentals, Strings, Arrays
  */
 public class FakeBinary {
 
+    public static void main(String[] args) {
+        String s = "12345678910";
+        System.out.println(fakeBinary("123456789"));
+        System.out.println(fakeBinary("9876543210"));
+    }
 
+    public static String fakeBinary(String s) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            int each = Integer.parseInt(s.substring(i,i+1));
+            if (each<5) {
+                result.append(0);
+            } else result.append(1);
+        }
+       return result.toString();
+    }
 }
